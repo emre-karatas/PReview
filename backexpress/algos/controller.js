06ad0b5184c330f066db355426e3b4bbca7dff05
Zeynep <zeynep.h.akgul@gmail.com>
2024-05-04 14:43:54 +0300

    getAllPullRequests in whole repo
    to be tested

--- backexpress/algos/controller.js
+++ backexpress/algos/controller.js
@@ -130,6 +130,30 @@ async function countPRReviewCommentsPerDeveloper(owner, repo) {
     return rankedDevelopers;
 }
 
+async function getAllPullRequests(owner, repo) {
+    let allPullRequests = [];
+    let apiUrl = `https://api.github.com/repos/${owner}/${repo}/pulls?state=all&per_page=100`;
+
+    while (apiUrl) {
+        const response = await axios.get(apiUrl, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/vnd.github.v3+json'
+            }
+        });
+
+        const pullRequests = response.data;
+        allPullRequests = allPullRequests.concat(pullRequests);
+
+        const links = response.headers['link'];
+        const nextLink = links && links.split(',').find(s => s.includes('rel="next"'));
+        apiUrl = nextLink ? new URL(nextLink.split(';')[0].trim().slice(1, -1)).toString() : null;
+    }
+
+    return allPullRequests;
+}
+
+
 // Express route handler for getting PR review comments count per developer
 router.post('/countPRReviewComments', async (req, res) => {
     const { owner, repo } = req.body;
