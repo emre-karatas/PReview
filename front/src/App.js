d1c4ae9f97c501f324a7342ad4732701f29c2762
beyzacaglar <102811663+beyzacaglar@users.noreply.github.com>
2024-05-06 02:50:09 +0300

    fix/ profile route fix

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import SignUp from './SignUp';
 import API from './API';
+import Profile from './Profile';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/api' element={<API />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </Router>
   );
