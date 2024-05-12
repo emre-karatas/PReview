709585e98ca67d8e2cf08a1b3c3b4bb9bc49c05a
beyzacaglar <102811663+beyzacaglar@users.noreply.github.com>
2024-05-12 18:25:28 +0300

    login functionality

--- front/src/Componenets/LoginForm/LoginForm.jsx
+++ front/src/Componenets/LoginForm/LoginForm.jsx
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import './LoginForm.css';
 import { FaUser, FaLock } from "react-icons/fa";
+import { loginUser } from "../../api/authAdapter";
 
 const LoginForm = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await loginUser({ username, password });
+            console.log("Login Response:", response.message);
+        } catch (error) {
+            setError(error.message);
+        }
+    }
+
     return (
         <div className="wrapper">
-            <form action="">
+            <form onSubmit={handleLogin}>
                 <h1 className="preview-name">PReview</h1>
                 <h3>Login</h3>
 
                 <div className="input-box">
-                    <input type="text" placeholder="Username" required />
+                    <input 
+                        type="text" 
+                        placeholder="Username" 
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required 
+                    />
                     <FaUser className="icon" />
                 </div>
 
                 <div className="input-box">
-                    <input type="password" placeholder="Password" required />
+                    <input 
+                        type="password" 
+                        placeholder="Password" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required 
+                    />
                     <FaLock className="icon" />
                 </div>
 
@@ -28,19 +56,22 @@ const LoginForm = () => {
                 </div>
 
 
-                <button type="submit">
+                <button 
+                    type="submit"
+                >
                     Login
                 </button>
 
-                <div className="register-link">
-                    <p>
-                        Don't have an acoount? 
-                        <a href="/signup">
-                            Register
-                        </a>
-                    </p>
-                </div>
             </form>
+
+            <div className="register-link">
+                <p>
+                    Don't have an acoount? 
+                    <a href="/signup">
+                        Register
+                    </a>
+                </p>
+            </div>
         </div>
     );
 };
