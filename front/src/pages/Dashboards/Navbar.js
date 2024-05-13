50ba7ad32dc78ab1d63c8bf5b523b919d9c66243
Emre Karatas <85746327+emre-karatas@users.noreply.github.com>
2024-05-13 12:38:54 +0300

    fix: frontend sidebar and navbar

--- front/src/pages/Dashboards/Navbar.js
+++ front/src/pages/Dashboards/Navbar.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './Navbar.css';
-import { FaBell, FaCog } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="search-bar">
         <input type="text" placeholder="Search (Ctrl+/)" />
-      </div>
-      <div className="icons">
-        <FaBell className="icon" />
-        <FaCog className="icon" />
       </div>
     </nav>
   );
